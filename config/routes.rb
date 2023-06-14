0a9a229e4165b22988fba3c5ab68e7942472fdee
Marco Andrade <marcoantonio.andrade05@gmail.com>
2023-06-14 17:26:28 -0300

    Refatoração Biblioteca 14/06

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  #devise_for :users
+  get 'dashboard/index'
+  resources :books
+  resources :themes
+  resources :authors
+  resources :employers
+  resources :students
+  resources :welcome
+
+  root to: 'welcome#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
